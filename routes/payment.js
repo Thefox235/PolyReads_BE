e3cf8d8f9ff2ee3e00d262582c770d5b7039539d
KT <thanhduyhf2@gmail.com>
2025-04-17 23:16:27 +0700

    //request

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-const request = require('request');
+// const request = require('request');
 const moment = require('moment');
 const paymentController = require('../mongo/controller.model');
 const Payment = require('../mongo/payment.model');
