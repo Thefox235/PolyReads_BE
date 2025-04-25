01fb1da0581def8aa6fe5b20cfbe8b76e0c80abf
KT <thanhduyhf2@gmail.com>
2025-04-25 21:27:41 +0700

    fix vnpay_done

--- routes/payment.js
+++ routes/payment.js
@@ -16,11 +16,11 @@ const dotenv = require('dotenv');
 // const VNP_API = process.env.vnp_Api || "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 // const VNP_RETURN_URL = process.env.vnp_ReturnUrl || "https://poly-reads.vercel.app/vnpay_return";
 
-const VNP_TMN_CODE = "5IVE3QI3";
-const VNP_HASH_SECRET =  "4E27C80SH223IRY4LDTGZN11I0AUP3M3";
-const VNP_PAY_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-const VNP_API = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
-const VNP_RETURN_URL = "https://poly-reads.vercel.app/vnpay_return";
+const VNP_TMN_CODE ="5IVE3QI3";
+const VNP_HASH_SECRET ="0MXMLPKKEZ9PV9ERLZEBZ25FGPF4OEEE";
+const VNP_PAY_URL ="https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+const VNP_API ="https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
+const VNP_RETURN_URL ="https://poly-reads.vercel.app//paymentResult";
 
 // Cấu hình ứng dụng (sandbox)
 const config = {
